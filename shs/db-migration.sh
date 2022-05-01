fb707fafbdd253a7f05f05a1440dd61b10305322
Msubetir <msubetir@gmail.com>
2022-04-30 23:00:23 -0300

    test

--- shs/db-migration.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Run Laravel migration (by force, since it would be a prod-environment)
-php artisan migrate --force
---
+++ src/scripts/migration.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+php artisan migrate --force
+php artisan db:seed --force
