c623ea3c4593d88a45381756010a5232257f8a3a
Msubetir <msubetir@gmail.com>
2022-05-01 22:14:18 -0300

    layout change

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -45,13 +45,13 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::is('register'))
+                            @if (!Route::is('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
-                            @if (Route::is('login'))
+                            @if (!Route::is('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
