8e88f575538c680c40b06945e28ade4134020389
Msubetir <msubetir@gmail.com>
2022-05-09 16:42:48 -0300

    copyright

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
+<body style="height: 100vh" class="d-flex justify-content-between flex-column">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -69,6 +69,12 @@
         <main class="py-4">
             @yield('content')
         </main>
+    </div>
+    
+    <div class="card">
+        <div class="card-body text-center">
+            © {{ date("Y") }} Maycon S. and Grape team 
+        </div>
     </div>
 </body>
 </html>
--- src/resources/views/layouts/start.blade.php
+++ src/resources/views/layouts/start.blade.php
@@ -21,7 +21,7 @@
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
+<body style="height: 100vh" class="d-flex justify-content-between flex-column">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -53,6 +53,12 @@
         <main class="py-4">
             @yield('content')
         </main>
+    </div>
+
+    <div class="card">
+        <div class="card-body text-center">
+            © {{ date("Y") }} Maycon S. and Grape team 
+        </div>
     </div>
 </body>
 </html>
